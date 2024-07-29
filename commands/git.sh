8d889b63ea2c2f92fcbc20eba31b0bf48a1b1e2b
alejandroBetancourth <lbetancourth@soaint.com>
2024-07-29 08:42:22 -0500

    test run on 29/07/2024/08:42:21

--- commands/git.sh
+++ commands/git.sh
@@ -2,14 +2,20 @@
 BRANCH_NAME="test-$(date +%d/%m/%Y-%N)"
 
 git checkout -b $BRANCH_NAME
-
+echo '-----------------------------'
 git add .
+echo '-----------------------------'
 git commit -m "test run on $(date +%d/%m/%Y/%H:%M:%S) "
+echo '-----------------------------'
 
 git push origin $BRANCH_NAME
+echo '-----------------------------'
 
 git checkout main
+echo '-----------------------------'
 
 git merge $BRANCH_NAME
+echo '-----------------------------'
 
 git push origin main
+echo '-----------------------------'
